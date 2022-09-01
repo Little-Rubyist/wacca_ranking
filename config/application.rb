437076eee59d02649d68b16c06c92ea12c1f841f
SenaR <Lily_white.purity@outlook.com>
2022-09-02 01:17:45 +0900

    i18n追加

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,8 @@ module Myapp
     # config.eager_load_paths << Rails.root.join("extras")
     config.autoload_paths += %W[#{config.root}/lib/]
     config.autoload_paths += %W[#{config.root}/lib/scripts]
+
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
