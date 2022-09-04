fe2115b88302227d27ea495a51ee984d3bc1d021
SenaR <Lily_white.purity@outlook.com>
2022-09-05 02:04:28 +0900

    ログインに使うデータを変更

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
+    user = User.find_by(name: params[:session][:name].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to root_url
+      redirect_to user_url(user)
     else
       render 'new'
     end
