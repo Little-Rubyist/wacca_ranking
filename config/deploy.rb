48e2992f6b61c04f8a8d293b9b4aa8c24048f715
AioiY <Little-Rubyist@outlook.com>
2022-09-28 05:24:53 +0900

    setting capfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.17.1"
 
-set :application, "my_app_name"
+set :application, "wacca_ranking"
 set :repo_url, "git@example.com:me/my_repo.git"
 
 # Default branch is :master
