880c870b9fc5ea48be9baec2bcaaf477a6c62648
SenaR <Lily_white.purity@outlook.com>
2022-09-27 22:56:03 +0900

    User#show 修正

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,2 +1,10 @@
 module SongsHelper
+  def show_archive_text(song)
+    achieve = @scores.find{ |x| x.user_song_id === song.id }&.achieve
+    Song.human_attribute_enum_value(:achieve, achieve)
+  end
+
+  def show_score(song)
+    @scores.find{ |x| x.user_song_id === song.id }&.score
+  end
 end
