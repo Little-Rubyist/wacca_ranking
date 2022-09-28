8232e8d2fa503611e883f42525cc86aeac7d8b5f
AioiY <Little-Rubyist@outlook.com>
2022-09-28 19:06:49 +0900

    IDを大文字認識しないようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_many :user_songs
   has_many :songs, through: :user_songs
   has_secure_password validations: true
+  before_save { name.downcase! }
 
   validates :name, presence: true, uniqueness: true
 end
