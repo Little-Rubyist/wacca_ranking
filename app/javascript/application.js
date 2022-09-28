10671edf7578d9973ebd74543f32463dee03f3cb
AioiY <Little-Rubyist@outlook.com>
2022-09-28 22:49:21 +0900

    ログインしていない状態の楽曲一覧に検索機能を追加

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,18 +3,25 @@ import "popper"
 import "bootstrap"
 import Rails from '@rails/ujs';
 Rails.start();
-console.log('yeah, we are ready!')
+
 window.updateTextInput = function(val) {
   document.getElementById('difficulty-value-id').value = `${val}以上`
 }
 
 window.resetDifficulty = function () {
   document.getElementById('q_song_difficulty_gteq').value = 1
+  document.getElementById('q_difficulty_gteq').value = 1
   document.getElementById('difficulty-value-id').value = '未選択'
 }
 
 if (document.getElementById('q_song_difficulty_gteq').value > 1) {
   updateTextInput(document.getElementById('q_song_difficulty_gteq').value)
+} else {
+  resetDifficulty()
+}
+
+if (document.getElementById('q_difficulty_gteq').value > 1) {
+  updateTextInput(document.getElementById('q_difficulty_gteq').value)
 } else {
   resetDifficulty()
 }
