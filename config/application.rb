f952b3addd76f88fc8d203fa7ff0404245f55072
AioiY <Little-Rubyist@outlook.com>
2022-09-29 07:31:25 +0900

    サブドメインに変更 dumperへの誘導追加 タイトル変更

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module Myapp
 
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.time_zone = 'Tokyo'
   end
 end
