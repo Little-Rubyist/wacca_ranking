62103b04d7399cda27228aa77a9c80c5ee8cb266
Little-Rubyist <Little-Rubyist@outlook.com>
2022-12-14 16:20:18 +0900

    add test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:add_user) { build(:user, name: 'test') }
-  it 'should unique name' do
-    create(:user, name: 'test')
-    expect(add_user.valid?).to eq false
+  let(:add_user) { build(:user, name: 'Test') }
+  let(:valid_user) { build(:user, name: 'test-user_name123') }
+  let(:invalid_user) { build(:user, name: 'test@')}
+  describe 'create' do
+    before do
+      create(:user, name: 'test')
+    end
+    it 'should unique name' do
+      expect(add_user.valid?).to eq false
+    end
+
+    it 'should have alphabet, number, hyphen or underscore only' do
+      expect(valid_user.valid?).to eq true
+      expect(invalid_user.valid?).to eq false
+    end
   end
 end
