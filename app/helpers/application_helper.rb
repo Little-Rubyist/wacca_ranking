3c7fe0a1b88b0e4d7a67140709f466a83354c7a0
Yla Aioi <43639533+Little-Rubyist@users.noreply.github.com>
2022-12-20 13:35:59 +0900

    IDを編集できるようにしました (#33)
    * fix validation and add form
    
    add regex validation to user.name
    fix locale method
    can update user.name when logged in
    
    * add test
    
    Co-authored-by: Little-Rubyist <Little-Rubyist@outlook.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,9 @@ module ApplicationHelper
   def locale_path(locale)
     uri = Addressable::URI.parse(request.url)
     params = uri.query_values
+    if params.nil?
+      params = { 'locale'=> 'ja' }
+    end
     params['locale'] = locale
     uri.query_values = params
     uri.to_s
