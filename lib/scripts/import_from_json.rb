e4106a2fb9b49e8fa92af55c6914959818356fa6
Little_Rubyist <Little_Rubyist@outlook.com>
2023-02-10 21:04:46 +0900

    お気に入り曲情報をJSONからインポート出来るようにした

--- lib/scripts/import_from_json.rb
+++ lib/scripts/import_from_json.rb
@@ -4,6 +4,17 @@ class ImportFromJson
     @json = JSON.parse(File.read(file))['player']
   end
 
+  def import_favorites
+    update_ids = []
+    @json['favorites'].each do |id|
+      update_ids << UserSong.includes(:song)
+                            .where(song: {music_id: id})
+                            .where(user: @user)
+                            .map(&:id)
+    end
+    UserSong.where(id: update_ids.flatten).update_all(is_favorite: true)
+  end
+
   def import_trophies
     @json['trophies'].each do |version|
       version.filter{|trophy| trophy['unlocked']}.map{|trophy| @user.trophy << trophy['id']}
